37ae4b757cf4cdfdd5bc09f95d099d5810e9162e
Bobbi York <york1670@gmail.com>
2018-10-11 19:44:34 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,8 +26,9 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-end
 
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun poweder"] = 4
-return shipping_manifest
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun poweder"] = 4
+  return shipping_manifest
+
+end
